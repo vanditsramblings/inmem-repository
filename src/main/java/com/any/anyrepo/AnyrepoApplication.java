916718b734d3ea6fbd71002e1374734ce4c551e9
Humble <vanditsharma001@gmail.com>
2022-05-01 22:56:02 -0700

    Initialized spring repo

---
+++ src/main/java/com/any/anyrepo/AnyrepoApplication.java
@@ -0,0 +1,13 @@
+package com.any.anyrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/any/anyrepo/AnyrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.any.anyrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
