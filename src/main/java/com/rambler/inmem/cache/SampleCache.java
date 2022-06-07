5bc8963d9bbdc9f8d5e2584c50de47193e1e5d92
humblemojo <vanditsharma001@gmail.com>
2022-06-06 23:51:08 -0700

    --Minor javadoc updates

--- src/main/java/com/rambler/inmem/cache/SampleCache.java
+++ src/main/java/com/rambler/inmem/cache/SampleCache.java
@@ -3,6 +3,10 @@ package com.rambler.inmem.cache;
 import com.rambler.inmem.entity.SampleEntity;
 import com.rambler.inmem.repository.impl.GenericKeyValueRepository;
 
+/**
+ * A sanmple entity to illustrate repository usage
+ * @author vandit
+ */
 public class SampleCache extends GenericKeyValueRepository<String, SampleEntity> {
 
     public SampleCache(String name, int ttl, int size) {
